7d52f9a274128067649b79488348bcd0ac799405
Anna L Henderson <annahenderson@Annas-iMac.lan>
2021-04-10 21:13:54 +0100

    simple encryption using mongoose-encryption

---
+++ app.js
@@ -0,0 +1,77 @@
+//jshint esversion:6
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
+
+const app = express();
+
+app.use(express.static("public"));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+mongoose.connect("mongodb://localhost:27017/userDB", { useUnifiedTopology: true, useNewUrlParser: true});
+
+const userSchema = new mongoose.Schema({
+    email: String,
+    secretword: String
+});
+
+const secret = "thisisourlittlesecret";
+userSchema.plugin(encrypt, {secret: secret, encryptedFields: ["secretword"]}) // encryptedFields ensures that only the secretword is encrypted, rather than the whole database
+
+const User = new mongoose.model("User", userSchema);
+
+app.get("/", function(req, res){
+    res.render("home");
+});
+
+// /login route
+app.get("/login", function(req, res){
+    res.render("login");
+});
+
+app.post("/login", function(req, res){
+    const username = req.body.username;
+    const secretword = req.body.secretword;
+
+    User.findOne({email: username}, function(err, foundUser){
+        if (err){
+            console.log(err);
+        } else {
+            if (foundUser) {
+                if(foundUser.secretword === secretword) {
+                    res.render("secrets");
+                }
+            }
+        }      
+    })
+})
+
+// /register route
+app.get("/register", function(req, res){
+    res.render("register");
+});
+
+app.post("/register", function(req, res){
+    
+    const newUser = new User({
+        email: req.body.username,
+        secretword: req.body.secretword,
+    });
+
+    newUser.save(function(err){
+        if (err) {
+            console.log(err);
+        } else {
+            res.render("secrets");
+        }
+    });
+});
+
+app.listen(3000, function(){
+    console.log("server started on port 3000.");
+})
